3f36960bcb6e0dbd65e8940127e4ffb2fbb7f9ef
Student <chadharris2002@gmail.com>
2022-09-27 10:52:39 -0400

    We did it

---
+++ script.js
@@ -0,0 +1,53 @@
+// 1
+console.log(13);
+
+// 2
+// Number, String, Boolean, Null, Undefined
+
+// 3
+// Remainder/modulus %
+// Addition + 
+// Subtraction -
+// Division /
+// Multiplication *
+// Exponentiation **
+
+// Extra:
+// Increment ++
+// Decrement --
+
+// 4
+console.log(5 % 2);
+console.log(5 % 4);
+// 5
+console.log('I\'M MARY POPPINS\, Y\'ALL!');
+
+// 6
+console.log('Hawk' + 'eye');
+// 7
+console.log('Chad'.length + 'harris'.length);
+// 8
+let avengers = "Avengers: Endgame";
+console.log(avengers[16]);
+// bonus
+// 1
+console.log("\/");
+// 2
+console.log(5 ** 4 - 9)
+// 3
+console.log("I".length - "Am".length + "Iron".length - "man".length);
+console.log(("I".length + "Am".length + "Iron".length + "man".length) % 10);
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+// 5
+let m = 25;
+let c = 4;
+let u = m + c;
+console.log(u)
